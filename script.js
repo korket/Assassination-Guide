1113a02b8ca3c7407a59eabc3a74d23f7f33860d
korket <korket1234@gmail.com>
2024-11-02 20:04:04 +0700

    add explanation for [d]

--- script.js
+++ script.js
@@ -5,8 +5,9 @@ const ability = document.querySelectorAll('.ability');
 const ability_d = document.querySelectorAll('.ability-d');
 const button_d = document.querySelector('.button-d');
 
-ability_d[0].style.display = 'flex'; 
+ability_d[0].style.display = 'flex';
 ability_d[1].style.display = 'flex';
+ability_d[2].style.display = 'flex';
 
 function hide() {
     for (const abilities of ability) {
