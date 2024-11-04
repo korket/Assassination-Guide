3becde9f4b9b26130586f742c4d6511993205e03
korket <korket1234@gmail.com>
2024-11-04 10:41:06 +0700

    add equipments container placeholder

--- script.js
+++ script.js
@@ -27,21 +27,19 @@ function showAbility(button) {
 
 const equipment_buttons = document.querySelectorAll('.equipment-button');
 
-const button_neptinos = document.querySelector('.button-neptinos');
-const button_gnosis = document.querySelector('.button-gnosis');
-const button_alteia = document.querySelector('.button-alteia');
-const button_arcana = document.querySelector('.button-arcana');
-
-function addColor() {
+function removeColor() {
     for (const buttons of equipment_buttons) {
         buttons.style.backgroundColor = '';
     };
 };
 
 function showEquipment(button) {
-    addColor();
+    removeColor();
     const container = document.querySelectorAll('.button-'+button);
     for (const element of container) {
         element.style.backgroundColor = element.getAttribute ('color');
     };
 };
+
+// equipment-show
+
