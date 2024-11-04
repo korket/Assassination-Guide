e74e0e3fc7d21c40109170ce4f0d36e918e0d96d
korket <korket1234@gmail.com>
2024-11-04 17:28:44 +0700

    fix equipment grade buttons, explanation added

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 // abilities selector
 
 const ability = document.querySelectorAll('.ability');
-
 const ability_d = document.querySelectorAll('.ability-d');
 const button_d = document.querySelector('.button-d');
-
 ability_d[0].style.display = 'flex';
 ability_d[1].style.display = 'flex';
 ability_d[2].style.display = 'flex';
@@ -26,7 +24,6 @@ function showAbility(button) {
 // equipment-buttons
 
 const equipment_buttons = document.querySelectorAll('.equipment-button');
-
 const neptinos_button = document.querySelector('.button-neptinos');
 
 neptinos_button.style.backgroundColor = neptinos_button.getAttribute('color');
@@ -37,21 +34,33 @@ function removeColor() {
     };
 };
 
+const equipments = document.querySelectorAll('.equipments');
+
+function removeGrade() {
+    for (const equipment of equipments) {
+        equipment.style.display = 'none';
+    }
+}
+
 function showEquipments(button) {
     removeColor();
+    removeGrade();
     const container = document.querySelectorAll('.button-'+button);
+    const gradeContainer = document.querySelectorAll('.equipment-'+button);
     for (const element of container) {
         element.style.backgroundColor = element.getAttribute ('color');
+    };
+    for (const grade of gradeContainer) {
+        grade.style.display = 'flex';
     };
 };
 
 // equipment-show
 
 const equipment_container = document.querySelectorAll('.equipment-container');
-
 const equipment_weapon = document.querySelector('.equipment-weapon');
 
-equipment_weapon.style.display = 'flex';
+equipment_weapon.style.display = 'grid';
 
 function hideEquipment() {
     for (const equipments of equipment_container) {
@@ -63,6 +72,6 @@ function showEquipment(img) {
     hideEquipment();
     const container = document.querySelectorAll('.equipment-'+img);
     for (const element of container) {
-        element.style.display = 'flex';
+        element.style.display = 'grid';
     };
 };
